bc5760c76bc8f29d453f04b03024f6add87eb95a
Tung <quangtung.ng99@gmail.com>
2020-07-02 20:28:17 +0300

    Finished part 3

---
+++ notes_app/backend/build/precache-manifest.a09192b19022e6e380a7a31ed3ecaf5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8d40833fad0c4390ca545339c71817",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e71f2aa4752273d8338",
+    "url": "/static/css/main.252dd0d5.chunk.css"
+  },
+  {
+    "revision": "540c3ac799efed663832",
+    "url": "/static/js/2.ced01a30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ced01a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e71f2aa4752273d8338",
+    "url": "/static/js/main.a4df811f.chunk.js"
+  },
+  {
+    "revision": "402f05c59b4e2a57dec5",
+    "url": "/static/js/runtime-main.d1310a82.js"
+  }
+]);
