aa2c20ac2d9a41d01e8cd57597182390f284e7af
Tung <quangtung.ng99@gmail.com>
2020-07-04 22:11:08 +0300

    Added middlewares

--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -1,32 +1,21 @@
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-// app.get('/api/blogs', (request, response) => {
-//   Blog
-//     .find({})
-//     .then(blogs => {
-//       response.json(blogs)
-//     })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog
-//     .save()
-//     .then(result => {
-//       response.status(201).json(result)
-//     })
-// })
 app.use('/api/blogs', blogsRouter)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
